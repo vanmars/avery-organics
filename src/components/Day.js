de70dd50fdadbfe03b6e04ab6664e086ec3ca3f7
Vanessa Stewart <vstewart1213@gmail.com>
2020-11-17 10:41:15 -0800

    Add ProductList and Month components

--- src/components/Day.js
+++ src/components/Day.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-
 function Day(props){
   return(
     <React.Fragment>
---
+++ src/components/Month.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Month(props) {
+  return(
+    <React.Fragment>
+      <h4>{props.name}</h4>
+      <ul>
+        {props.selection.map((product) =>
+          <li>{product}</li>
+        )}
+       </ul>
+    </React.Fragment>
+  )
+}
+
+Month.propTypes = {
+  name: PropTypes.string,
+  selection: PropTypes.arrayOf(PropTypes.string)
+}
+
+export default Month;
